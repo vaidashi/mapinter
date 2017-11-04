393f2869adb2bcbe7cb0a698f69088fa7bf6896b
Ashish Vaid <vaid50@yahoo.com>
2017-11-04 14:32:08 -0600

    currency api broken into services and presenters

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -4,12 +4,7 @@ class RegionsController < ApplicationController
   def index
     @region = Region.all
 
-    response = Faraday.get("http://apilayer.net/api/live?access_key=#{ENV["CURRENCY_KEY"]}&currencies=AUD,EUR,GBP,JPY,CNY,HKD&format=1")
-
-    raw_rates = JSON.parse(response.body, symbolize_names: true)
-    # binding.pry
-
-    @currency_rates = Conversion.new(raw_rates[:quotes])
+    @currency_rates = CurrencyPresenter.find_rates
   end
 
 end
