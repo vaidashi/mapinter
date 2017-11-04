5de3c57e21644ded7bd8a1761c908a9a062c5dcf
Ashish Vaid <vaid50@yahoo.com>
2017-11-04 12:54:18 -0600

    added env variable to travis ci

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -13,23 +13,3 @@ class RegionsController < ApplicationController
   end
 
 end
-
-class Conversion
-
-  attr_reader :to_austrailian,
-              :to_euro,
-              :to_sterling,
-              :to_yen,
-              :to_yuan,
-              :to_hkd
-
-  def initialize(attrs)
-    @to_austrailian = attrs[:USDAUD]
-    @to_euro = attrs[:USDEUR]
-    @to_sterling = attrs[:USDGBP]
-    @to_yen = attrs[:USDJPY]
-    @to_yuan = attrs[:USDCNY]
-    @to_hkd = attrs[:USDHKD]
-  end
-
-end
