865adde576911a5a8a40cf754860900becfe8237
Ashish Vaid <vaid50@yahoo.com>
2017-11-04 17:11:45 -0600

    skipping flights api feature for now, parsing nytimes news feed instead

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -3,6 +3,11 @@ class CountriesController < ApplicationController
 
   def show
     @country = Country.find(params[:id])
+    response = Faraday.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=#{ENV["NEWS_KEY"]}&q=#{@country.name}&sort=newest&page=0&fq=news_desk:(Travel)ANDglocations:(#{@country.name})")
+
+    raw_news = JSON.parse(response.body, symbolize_names: true)
+    # binding.pry
+
   end
 
 end
