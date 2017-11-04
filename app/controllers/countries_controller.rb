adcda36d5b4ff787b49c3cc62c534d4514a6df4d
Ashish Vaid <vaid50@yahoo.com>
2017-11-04 17:34:12 -0600

    created test for news feed to show on country show page, getting expecting error

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -5,9 +5,11 @@ class CountriesController < ApplicationController
     @country = Country.find(params[:id])
     response = Faraday.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=#{ENV["NEWS_KEY"]}&q=#{@country.name}&sort=newest&page=0&fq=news_desk:(Travel)ANDglocations:(#{@country.name})")
 
-    raw_news = JSON.parse(response.body, symbolize_names: true)
+    raw_news = JSON.parse(response.body, symbolize_names: true)[:response][:docs]
     # binding.pry
-
+    # @news_feeds = raw_news.map do |raw_story|
+    #   News.new(raw_story)
+    # end
   end
 
 end
---
+++ spec/features/user_can_see_news_feed_for_country_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature "User visits country show page" do
+  scenario "and sees news feed for country" do
+    VCR.use_cassette("find_country_news") do
+    region = Region.create(name: "Austrailia/Oceania")
+    country = Country.create(name: "New Zealand", region_id: region.id)
+
+    visit region_country_path(region, country)
+
+    expect(page).to have_content(country.name)
+
+    expect(page).to have_css(".news_feed")
+    within(first(".news_feed")) do
+        expect(page).to have_css(".url")
+        expect(page).to have_css(".headline")
+      end
+    end 
+  end
+
+end
