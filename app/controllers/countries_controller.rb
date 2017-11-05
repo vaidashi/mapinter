c5e1ae7a364c621976bb4bc4b3eed3459973e9d6
Ashish Vaid <vaid50@yahoo.com>
2017-11-05 15:54:24 -0700

    added call for travel requirements

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -5,6 +5,14 @@ class CountriesController < ApplicationController
     @country = Country.find(params[:id])
 
     @news_feeds = NewsPresenter.new(@country).find_news
+
+
+    response = Faraday.get("https://travelbriefing.org/#{@country.name}?format=json")
+
+    raw_requirements = JSON.parse(response.body, symbolize_names: true)
+    # binding.pry
+
+    @travel_requirements = TravelRequirements.new(raw_requirements)
   end
 
 end
