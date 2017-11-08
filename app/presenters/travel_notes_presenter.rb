f7dc9d6d10c6c8a601f3a0bd0866db7c60e3ec09
Ashish Vaid <vaid50@yahoo.com>
2017-11-07 17:18:40 -0700

    added parameterize for travel briefing call, still need to figure how to split on caps , and , the, without returing it as array

--- app/presenters/travel_notes_presenter.rb
+++ app/presenters/travel_notes_presenter.rb
@@ -8,7 +8,7 @@ class TravelNotesPresenter
 
   def find_advisories
     raw_requirements = AdvisoryFinder.new(@country).find_travel_advice_call
-
+# binding.pry
     travel_requirements = TravelRequirements.new(raw_requirements)
   end
 
--- app/services/advisory_finder.rb
+++ app/services/advisory_finder.rb
@@ -2,11 +2,11 @@
 class AdvisoryFinder
 
   def initialize(country)
-    @conn = Faraday.get("https://travelbriefing.org/#{country.name}?format=json")
+    @conn = Faraday.get("https://travelbriefing.org/#{country.name.parameterize}?format=json")
   end
 
   def find_travel_advice_call
     JSON.parse(@conn.body, symbolize_names: true)
   end
-  
+
 end
